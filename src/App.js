39e8464f599ffc8a3e86f30571e491692d7a860e
jh0956 <junho0956@naver.com>
2020-10-13 16:28:48 +0900

    create Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      Hello start project!
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
